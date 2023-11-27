b80fb71f357daa91425f02c89f5eaf6b32462b57
francielefranca <francafrancielefranca@gmail.com>
2023-11-27 18:05:36 -0300

    commit inicial

---
+++ estacoes.py
@@ -0,0 +1,12 @@
+estacao = input("Digite uma estacao:")
+
+if estacao == "primavera":
+    print("As flores estao nascendo.")
+elif estacao == "verao":
+    print("Ah, o calor! Mergulhe em uma piscina.")
+elif estacao == "outono":
+    print("As folhas estao caindo...")
+elif estacao == "inverno":
+    print("Chuvas e ventos.")
+else:
+    print("Voce nao digitou uma estacao :( ")
